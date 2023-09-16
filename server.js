f4fef3b583db3644d01502f31986935d3db218f3
eduardobnhr <eduardobnhr@gmail.com>
2023-09-16 11:39:56 -0300

    create server native

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 import { createServer } from "node:http";
 
+const server = createServer((request, response) => {
+    response.write('hello world')
+
+
+    return response.end()
+})
+
+server.listen(3333)
