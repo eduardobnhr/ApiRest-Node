2402ba0c2c0ad2b43db6c4f14a9d986cbee84b2e
eduardobnhr <eduardobnhr@gmail.com>
2023-09-16 11:51:59 -0300

    add fastify in project

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
 
-const server = createServer((request, response) => {
-    response.write('hello world')
+// const server = createServer((request, response) => {
+//     response.write('hello world')
 
 
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import {fastify} from 'fastify'
+
+const server = fastify()
+
+
+server.get('/', ()=> {
+    return 'hello world'
+})
+server.get('/hello', ()=> {
+    return 'hello teste'
+})
+server.get('/node', ()=> {
+    return 'hello node'
 })
 
-server.listen(3333)
+
+server.listen({
+    port: 3333,
+})
